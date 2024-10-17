e51cd6af775473c645f7e822b95249e0e9daaa6e
Varsha Kandi <kandivarsha09@gmail.com>
2024-10-17 17:55:04 -0500

    Updates to Products page

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -2,13 +2,12 @@ import React from "react";
 export const Products = () => {
   return (
     <div>
-      <div className="text-white">
-        <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-          <h1 className="md:text-3xl sm:text-3xl text-5xl font-bold md:py-3">
-            PRODUCTS.
+        <div className="m-[50px] gap-6 justify-center text-white text-center">
+          <h1 className="md:text-3xl sm:text-3xl text-5xl font-bold md:py-3 text-[#00df9a]">
+            OUR PRODUCTS
           </h1>
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Quod quo, non omnis quia blanditiis rem fuga ipsa debitis asperiores sed aperiam praesentium repellat delectus aliquid velit, totam dolorum incidunt, deserunt laboriosam labore vitae unde veritatis? Officia maxime sapiente eligendi laboriosam, modi sed voluptas deserunt in repudiandae beatae autem ullam mollitia repellendus voluptate nemo neque unde quidem, aliquid enim veniam culpa. Dolor ducimus quo excepturi temporibus vero beatae? Tempora consectetur similique repudiandae expedita animi minus sunt voluptatibus quisquam porro, atque numquam eaque recusandae eius magni ea, iure, modi eos sed delectus dicta amet sint? Minima, veritatis? Laboriosam iusto illum ipsum et.</p>        
         </div>
-      </div>
     </div>
   );
 };
