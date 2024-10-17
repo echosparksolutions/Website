793bbdedb4179a362eade5a9b7555abe907bbe10
Varsha Kandi <kandivarsha09@gmail.com>
2024-10-17 17:23:54 -0500

    Updates to Contact Us page and added a link to the Website title

--- src/Pages/ContactUs.jsx
+++ src/Pages/ContactUs.jsx
@@ -1,15 +1,27 @@
 import React from "react";
+//max-w-[800px] mt-[-96px] w-full h-screen mx-auto 
 export const ContactUs = () => {
   return (
-    <div>
-      <div className="text-white">
-        <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
-          <p className="text-[#00df9a] font-bold p-3">
-            CONTACT US.
-          </p>
+        <div className="mt-[50px] flex flex-row mx-auto gap-6 justify-center">
+          <div className="w-[500px] ">
+            <h1 className="text-[#00df9a] font-bold">
+              CONTACT US
+            </h1>
+            <p className="my-4 text-white">
+              We’re here to assist you! Any queries you may have will be eagerly addressed by our specialists. Please get in touch with any of our global offices, and we’ll get back to you shortly.
+            </p>
+          </div>
+          <div className="w-[500px]">
+            <form className="flex flex-col gap-3">
+              <input type="text" placeholder="Name" className="p-3 border rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-[#00df9a] focus:border-transparent"></input>
+              <input type="text" placeholder="Email" className="p-3 border rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-[#00df9a] focus:border-transparent"></input>
+              <textarea type="text" placeholder="Message" className="p-3 border rounded-lg text-black focus:outline-none focus:ring-2 focus:ring-[#00df9a] focus:border-transparent"></textarea>
+              <button className='bg-[#00df9a] w-[200px] rounded-md font-medium my-6 mx-auto py-3 text-black uppercase hover:opacity-85'>
+                Send message
+              </button>
+            </form>
+          </div>
         </div>
-      </div>
-    </div>
   );
 };
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
 import React, {useState} from "react";
 import {AiOutlineClose, AiOutlineMenu} from 'react-icons/ai'
-import {Link } from 'react-router-dom'
+import {Link} from 'react-router-dom'
 const Navbar = () => {
     const [nav, setNav]= useState(false)
 
@@ -11,7 +11,7 @@ const Navbar = () => {
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
       <h1 className='w-full text-3xl font-bold text-[#00df9a]'>
-        EchoSparkSolutions
+        <Link to="/">EchoSparkSolutions</Link>
       </h1>
       <ul className='hidden md:flex'>
         <Link to="/" className="p-4">Home</Link>
